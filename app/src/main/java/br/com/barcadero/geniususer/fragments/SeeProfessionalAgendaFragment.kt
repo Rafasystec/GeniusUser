6362c41a383feea7bfd1d02af399f43431ceb571
rafasystec <rafasystec@yahoo.com.br>
2018-06-07 23:01:26 -0300

    Concluding the agenda screen layout.

--- app/src/main/java/br/com/barcadero/geniususer/fragments/SeeProfessionalAgendaFragment.kt
+++ app/src/main/java/br/com/barcadero/geniususer/fragments/SeeProfessionalAgendaFragment.kt
@@ -6,12 +6,15 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import br.com.barcadero.geniususer.R
 import br.com.barcadero.geniususer.android.adapters.SeeProfessionalAgendaAdapter
 import br.com.barcadero.geniususer.extensions.defaultRecycleView
 import br.com.barcadero.geniususer.extensions.toast
 import br.com.barcadero.geniususer.model.BaseEntity
 import br.com.barcadero.geniususer.model.ProfessionalAgenda
+import kotlinx.android.synthetic.main.fragment_see_professional_agenda.*
 import org.jetbrains.anko.alert
 
 
@@ -31,6 +34,10 @@ class SeeProfessionalAgendaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recycleView = defaultRecycleView(view,R.id.rcSeeAgendaProfessional)
+        initSpinnerAgendaDay()
+        fabHelpAgenda.setOnClickListener{
+            toast("Ajuda a caminho",activity!!)
+        }
     }
 
     override fun onResume() {
@@ -41,34 +48,26 @@ class SeeProfessionalAgendaFragment : Fragment() {
 
 
         var agenda = ProfessionalAgenda()
-        agenda.hour = "13:00"
+        agenda.hour = "08:00"
         agenda.isBusy = false
 
         var agenda1 = ProfessionalAgenda()
-        agenda1.hour = "14:00"
+        agenda1.hour = "11:00"
         agenda1.isBusy = false
 
         var agenda2 = ProfessionalAgenda()
-        agenda2.hour = "15:00"
+        agenda2.hour = "14:00"
         agenda2.isBusy = true
 
         var agenda3 = ProfessionalAgenda()
-        agenda3.hour = "16:00"
+        agenda3.hour = "17:00"
         agenda3.isBusy = false
 
         var agenda4 = ProfessionalAgenda()
-        agenda4.hour = "17:00"
+        agenda4.hour = "20:00"
         agenda4.isBusy = true
 
-        var agenda5 = ProfessionalAgenda()
-        agenda5.hour = "18:00"
-        agenda5.isBusy = false
-
-        var agenda6 = ProfessionalAgenda()
-        agenda6.hour = "19:00"
-        agenda6.isBusy = false
-
-        var list = listOf(agenda,agenda1,agenda2,agenda3,agenda4,agenda5,agenda6)
+        var list = listOf(agenda,agenda1,agenda2,agenda3,agenda4)
         recycleView?.adapter = SeeProfessionalAgendaAdapter(list,{ agenda : ProfessionalAgenda -> onAgendaClick(agenda)})
     }
 
@@ -91,6 +90,21 @@ class SeeProfessionalAgendaFragment : Fragment() {
 
     fun onClickYes(agenda:BaseEntity){
         toast("Muito obrigado por confiar em nós.",activity!!)
+    }
+
+    private  fun initSpinnerAgendaDay(){
+        spChooseADay.adapter = ArrayAdapter<String>(activity,R.layout.support_simple_spinner_dropdown_item, listOf("Hoje","Amanhã","Depois de amanhã"))
+        spChooseADay.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //var value = position
+                //Put the position
+            }
+
+        }
     }
 
 }
---
+++ app/src/main/java/br/com/barcadero/geniususer/model/responses/ClientAgendaResponse.kt
@@ -0,0 +1,14 @@
+package br.com.barcadero.geniususer.model.responses
+
+/**
+ * Created by Rafael Rocha on 07/06/2018.
+ */
+class ClientAgendaResponse {
+
+    var professionalId = 0L
+    var clientId = 0L
+    var clientName= ""
+    var professionalName = ""
+    var professionalPhotoUrl = ""
+
+}
